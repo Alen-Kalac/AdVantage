4b78980c7ea3cefb36e10fec3412653a72b2c056
Alen-Kalac <alengames@live.com>
2023-08-17 18:12:59 +0200

    implemented mantine carousel

--- src/components/componentsSRB/Testimonials/index.jsx
+++ src/components/componentsSRB/Testimonials/index.jsx
@@ -1,67 +1,96 @@
-import React from "react";
-import Testimonial from "../Testimonal";
+import React, { useRef } from "react";
 import "./styles.scss";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
-import imgOne from './Logo/One.png'
-import imgTwo from './Logo/Two.png'
-import imgThree from './Logo/Three.png'
-import imgFour from './Logo/Four.png'
-import imgFive from './Logo/Five.png'
-
+import Autoplay from "embla-carousel-autoplay"; // Make sure you've installed this package
+import { Carousel } from "@mantine/carousel";
+import { createStyles, getStylesRef } from '@mantine/core';
+import imgOne from "./Logo/One.png";
+import imgTwo from "./Logo/Two.png";
+import imgThree from "./Logo/Three.png";
+import imgFour from "./Logo/Four.png";
+import imgFive from "./Logo/Five.png";
+import imgSix from "./Logo/Six.png";
+import imgSeven from "./Logo/Seven.png";
+import imgEight from "./Logo/Eight.png";
+import imgNine from "./Logo/Nine.png";
+import imgTen from "./Logo/Ten.png";
+import imgEleven from "./Logo/Eleven.png";
+import imgTwelve from "./Logo/Twelve.png";
+import imgThirteen from "./Logo/Thirteen.png";
+import imgFourteen from "./Logo/Fourteen.png";
+import imgFifteen from "./Logo/Fifteen.png";
 
 function Testimonials() {
+  const autoplay = useRef(Autoplay({ delay: 2000 })); // Adjust the delay as needed
+
+  const useStyles = createStyles(() => ({
+    controls: {
+      ref: getStylesRef('controls'),
+      transition: 'opacity 150ms ease',
+      opacity: 0,
+    },
+  }));
+
+  const { classes } = useStyles();
   return (
     <div className="testimonials-srb">
-      <Carousel
-        className="shadow-xl"
-        infiniteLoop="true"
-        emulateTouch="true"
-        autoPlay="false"
-        interval={3000}
-        showThumbs={false}
-        showIndicators={false}
-        showStatus={false}
-      >
-       <div className="imgContainer"> <img src={imgOne} alt="" /></div>
-       <div className="imgContainer"><img src={imgTwo} alt="" /></div> 
-       <div className="imgContainer"><img src={imgThree} alt="" /></div> 
-       <div className="imgContainer"><img src={imgFour} alt="" /></div> 
-       <div className="imgContainer"><img src={imgFive} alt="" /></div> 
-      </Carousel>
-
-      {/* <Carousel
-        className="shadow-xl"
-        infiniteLoop="true"
-        emulateTouch="true"
-        autoPlay="false"
-        interval={2000}
-        showThumbs={false}
-        showIndicators={false}
-        showStatus={false}
-      >
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-      </Carousel>
-      <Carousel
-        className="shadow-xl"
-        infiniteLoop="true"
-        emulateTouch="true"
-        autoPlay="false"
-        interval={2000}
-        showThumbs={false}
-        showIndicators={false}
-        showStatus={false}
-      >
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-      </Carousel> */}
+   <div className="carousel-container">
+   <Carousel
+   classNames={classes}
+slideSize="20%"
+slideGap="xl"
+loop
+plugins={[autoplay.current]} // Add the autoplay plugin
+withControls="false"
+draggable={false}
+>
+<Carousel.Slide> 
+  <img src={imgOne} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgTwo} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgThree} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgFour} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgFive} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgSix} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgSeven} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgEight} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgNine} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgTen} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgEleven} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgTwelve} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgThirteen} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgFourteen} alt="" />
+</Carousel.Slide>
+<Carousel.Slide>
+  <img src={imgFifteen} alt="" />
+</Carousel.Slide>
+{/* ...other slides */}
+</Carousel>
+   </div>
     </div>
   );
 }
