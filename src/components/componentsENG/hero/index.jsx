5e117d7cad07dd912d715d9d41ce9754643574e0
Alen-Kalac <alengames@live.com>
2023-10-19 16:06:49 +0200

    designed video container and logic for autoplay

--- src/components/componentsENG/hero/index.jsx
+++ src/components/componentsENG/hero/index.jsx
@@ -1,32 +1,42 @@
-import React from 'react';
-import './styles.scss'
-
-import avatar from '../../Images/Marko-Hero.png'
+import React, { useEffect, useRef } from "react";
+import "./styles.scss";
+import video from "../../Images/Video.mp4";
+import avatar from "../../Images/Marko-Hero.png";
 
 function Hero() {
-    return (
-      <section className="hero-en">
-        <div className="hero-content">
-          <div className="hero-content-left">
-            <p className="hero-text animated-onload">
-              <span>We make short-form videos</span>
-              
-              <span> for long-term goals</span>
-              
-            </p>
-            <div className="hero-line animated-onload"></div>
-            <a href="#calendly">
-              <button onClick="" className="hero-button animated-onload">
-                Book a call with us
-              </button>
-            </a>
-          </div>
-          <div className="hero-content-right">
-            <img loading='lazy' className="avatar" src={avatar} alt="" />
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  }, []);
+
+  return (
+    <section className="hero-en">
+      <div className="hero-content">
+        <div className="hero-content-left">
+          <p className="hero-text animated-onload">
+            <span>We make short-form videos</span>
+            <span> for long-term goals</span>
+          </p>
+          <div className="hero-line animated-onload"></div>
+          <a href="#calendly">
+            <button onClick="" className="hero-button animated-onload">
+              Book a call with us
+            </button>
+          </a>
+        </div>
+        <div className="hero-content-right">
+          <div className="video-wrapper">
+            <video ref={videoRef} autoPlay muted playsInline loop>
+              <source src={video} type="video/mp4" />
+            </video>
           </div>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
+}
 
-  export default Hero;
+export default Hero;
